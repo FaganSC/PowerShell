359dee6455c9b91267bceef137860907e806bcdc
Shawn Fagan (Marathon) <sfagan@marathonus.com>
2020-01-10 22:57:09 -0500

    Enable MFA for Office 365 Users

---
+++ AzureAD/Enable-MFAUsers.ps1
@@ -0,0 +1,19 @@
+Import-Module MSOnline
+Connect-MsolService
+
+$st = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+$st.RelyingParty = "*"
+$st.State = "Enabled"
+$sta = @($st)
+
+Import-Csv -Path .\SampleDataFiles\MFAUsers.csv -Header "UserPrincipalName" | ForEach-Object {
+    Write-Host -NoNewline "Enabling MFA for $($_.UserPrincipalName)"
+    Try{
+        Set-MsolUser -UserPrincipalName $user -StrongAuthenticationRequirements $sta
+        Write-Host -f "Green" " .....Done!"
+    } Catch {
+        Write-Host -f "Red" " .....Error!"
+    }
+}
+
+
