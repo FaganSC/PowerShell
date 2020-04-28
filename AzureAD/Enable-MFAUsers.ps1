4d6896a4d377a6f7e023b529a66f3c545c47facd
Shawn Fagan (Marathon) <sfagan@marathonus.com>
2020-04-28 08:17:15 -0400

    Updated Enable MFA Script

--- AzureAD/Enable-MFAUsers.ps1
+++ AzureAD/Enable-MFAUsers.ps1
@@ -4,16 +4,16 @@ if (Get-Module -ListAvailable -Name MSOnline) {
     Install-Module -Name MSOnline
 }
 Connect-MsolService
-
+Set-Location $PSScriptRoot
 $st = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
 $st.RelyingParty = "*"
-$st.State = "Enabled"
+$st.State = "Enforced"
 $sta = @($st)
 
-Import-Csv -Path .\SampleDataFiles\MFAUsers.csv -Header "UserPrincipalName" | ForEach-Object {
+Import-Csv -Path .\..\DataFiles\MFAUsers.csv -Header "UserPrincipalName" | ForEach-Object {
     Write-Host -NoNewline "Enabling MFA for $($_.UserPrincipalName)"
     Try{
-        Set-MsolUser -UserPrincipalName $user -StrongAuthenticationRequirements $sta
+        Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationRequirements $sta -PasswordNeverExpires $true
         Write-Host -f "Green" " .....Done!"
     } Catch {
         Write-Host -f "Red" " .....Error!"
